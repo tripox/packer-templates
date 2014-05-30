e646da3c20c42ea87977b5c4987829e7f89bcf0a
Juan Treminio <jtreminio@gmail.com>
2014-05-30 11:50:39 -0500

    Adds Parallels boxes

---
+++ ubuntu-14.04-x86_64/scripts/parallels.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Bail if we are not running inside Parallels.
+if [[ `facter virtual` != "parallels" ]]; then
+    exit 0
+fi
+
+# Install the Parallels Tools
+
+mkdir -p /mnt/parallels
+mount -o loop /home/vagrant/prl-tools.iso /mnt/parallels
+
+/mnt/parallels/install --install-unattended-with-deps --progress
+
+umount /mnt/parallels
+
+rm -rf /home/vagrant/prl-tools.iso
