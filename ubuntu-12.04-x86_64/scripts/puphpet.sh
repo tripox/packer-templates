3381c5c186d81684252c4c533c5e9f0d546851b2
Juan Treminio <jtreminio@gmail.com>
2014-07-17 16:14:51 -0500

    update-puppet.sh => install-puppet.sh; Adds install-ruby.sh call

--- ubuntu-12.04-x86_64/scripts/puphpet.sh
+++ ubuntu-12.04-x86_64/scripts/puphpet.sh
@@ -5,13 +5,16 @@ useradd -g www-data www-data
 
 mkdir -p '/.puphpet-stuff/shell'
 
-wget --quiet --tries=5 --connect-timeout=10 --no-check-certificate  -O '/.puphpet-stuff/shell/os-detect.sh' 'https://raw.github.com/puphpet/puphpet/master/src/Puphpet/MainBundle/Resources/views/manifest/puphpet/shell/os-detect.sh'
-wget --quiet --tries=5 --connect-timeout=10 --no-check-certificate  -O '/.puphpet-stuff/shell/initial-setup.sh' 'https://raw.github.com/puphpet/puphpet/master/src/Puphpet/MainBundle/Resources/views/manifest/puphpet/shell/initial-setup.sh'
-wget --quiet --tries=5 --connect-timeout=10 --no-check-certificate  -O '/.puphpet-stuff/shell/update-puppet.sh' 'https://raw.github.com/puphpet/puphpet/master/src/Puphpet/MainBundle/Resources/views/manifest/puphpet/shell/update-puppet.sh'
+wget --quiet --tries=5 --connect-timeout=10 --no-check-certificate  -O '/.puphpet-stuff/shell/os-detect.sh' 'https://raw.github.com/puphpet/puphpet/shell-file-refactor/src/Puphpet/MainBundle/Resources/views/manifest/puphpet/shell/os-detect.sh'
+wget --quiet --tries=5 --connect-timeout=10 --no-check-certificate  -O '/.puphpet-stuff/shell/initial-setup.sh' 'https://raw.github.com/puphpet/puphpet/shell-file-refactor/src/Puphpet/MainBundle/Resources/views/manifest/puphpet/shell/initial-setup.sh'
+wget --quiet --tries=5 --connect-timeout=10 --no-check-certificate  -O '/.puphpet-stuff/shell/install-ruby.sh' 'https://raw.github.com/puphpet/puphpet/shell-file-refactor/src/Puphpet/MainBundle/Resources/views/manifest/puphpet/shell/install-ruby.sh'
+wget --quiet --tries=5 --connect-timeout=10 --no-check-certificate  -O '/.puphpet-stuff/shell/install-puppet.sh' 'https://raw.github.com/puphpet/puphpet/shell-file-refactor/src/Puphpet/MainBundle/Resources/views/manifest/puphpet/shell/install-puppet.sh'
 
 chmod +x '/.puphpet-stuff/shell/os-detect.sh'
 chmod +x '/.puphpet-stuff/shell/initial-setup.sh'
-chmod +x '/.puphpet-stuff/shell/update-puppet.sh'
+chmod +x '/.puphpet-stuff/shell/install-ruby.sh'
+chmod +x '/.puphpet-stuff/shell/install-puppet.sh'
 
-. '/.puphpet-stuff/shell/initial-setup.sh' '/.puphpet-stuff'
-. '/.puphpet-stuff/shell/update-puppet.sh'
+source '/.puphpet-stuff/shell/initial-setup.sh' '/.puphpet-stuff'
+source '/.puphpet-stuff/shell/install-ruby.sh'
+source '/.puphpet-stuff/shell/install-puppet.sh'
