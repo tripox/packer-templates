0bf61c617fabcf96041fc846492563a1a5360c17
Juan Treminio <jtreminio@gmail.com>
2014-07-22 09:01:18 -0500

    Updates URLs from shell-file-refactor to master due to branch being merged in. Duplicates #1. Thanks @misilot

--- ubuntu-12.04-x86_64/scripts/puphpet.sh
+++ ubuntu-12.04-x86_64/scripts/puphpet.sh
@@ -5,10 +5,10 @@ useradd -g www-data www-data
 
 mkdir -p '/.puphpet-stuff/shell'
 
-wget --quiet --tries=5 --connect-timeout=10 --no-check-certificate  -O '/.puphpet-stuff/shell/os-detect.sh' 'https://raw.github.com/puphpet/puphpet/shell-file-refactor/src/Puphpet/MainBundle/Resources/views/manifest/puphpet/shell/os-detect.sh'
-wget --quiet --tries=5 --connect-timeout=10 --no-check-certificate  -O '/.puphpet-stuff/shell/initial-setup.sh' 'https://raw.github.com/puphpet/puphpet/shell-file-refactor/src/Puphpet/MainBundle/Resources/views/manifest/puphpet/shell/initial-setup.sh'
-wget --quiet --tries=5 --connect-timeout=10 --no-check-certificate  -O '/.puphpet-stuff/shell/install-ruby.sh' 'https://raw.github.com/puphpet/puphpet/shell-file-refactor/src/Puphpet/MainBundle/Resources/views/manifest/puphpet/shell/install-ruby.sh'
-wget --quiet --tries=5 --connect-timeout=10 --no-check-certificate  -O '/.puphpet-stuff/shell/install-puppet.sh' 'https://raw.github.com/puphpet/puphpet/shell-file-refactor/src/Puphpet/MainBundle/Resources/views/manifest/puphpet/shell/install-puppet.sh'
+wget --quiet --tries=5 --connect-timeout=10 --no-check-certificate  -O '/.puphpet-stuff/shell/os-detect.sh' 'https://raw.github.com/puphpet/puphpet/master/src/Puphpet/MainBundle/Resources/views/manifest/puphpet/shell/os-detect.sh'
+wget --quiet --tries=5 --connect-timeout=10 --no-check-certificate  -O '/.puphpet-stuff/shell/initial-setup.sh' 'https://raw.github.com/puphpet/puphpet/master/src/Puphpet/MainBundle/Resources/views/manifest/puphpet/shell/initial-setup.sh'
+wget --quiet --tries=5 --connect-timeout=10 --no-check-certificate  -O '/.puphpet-stuff/shell/install-ruby.sh' 'https://raw.github.com/puphpet/puphpet/master/src/Puphpet/MainBundle/Resources/views/manifest/puphpet/shell/install-ruby.sh'
+wget --quiet --tries=5 --connect-timeout=10 --no-check-certificate  -O '/.puphpet-stuff/shell/install-puppet.sh' 'https://raw.github.com/puphpet/puphpet/master/src/Puphpet/MainBundle/Resources/views/manifest/puphpet/shell/install-puppet.sh'
 
 chmod +x '/.puphpet-stuff/shell/os-detect.sh'
 chmod +x '/.puphpet-stuff/shell/initial-setup.sh'
