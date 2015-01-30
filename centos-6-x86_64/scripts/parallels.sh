e04bf1ba738aaa0faeb7a821a01b03e14bb26935
Juan Treminio <jtreminio@gmail.com>
2015-01-29 18:44:32 -0600

    Handles CentOS 6 and 7 repos using RELEASE value

--- centos-6-x86_64/scripts/parallels.sh
+++ centos-6-x86_64/scripts/parallels.sh
@@ -8,10 +8,10 @@
 # Install the Parallels Tools
 
 mkdir -p /mnt/parallels
-mount -o loop /home/vagrant/prl-tools.iso /mnt/parallels
+mount -o loop /home/vagrant/prl-tools-lin.iso /mnt/parallels
 
 /mnt/parallels/install --install-unattended-with-deps --progress
 
 umount /mnt/parallels
 
-rm -rf /home/vagrant/prl-tools.iso
+rm -rf /home/vagrant/prl-tools-lin.iso
