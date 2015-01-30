4e64bdd606b6e6b4cb279650c9fcc13afd39c037
Juan Treminio <jtreminio@gmail.com>
2015-01-30 14:28:28 -0600

    Updates mem and cpu; updates box url

--- ubuntu-14.04-x86/scripts/base.sh
+++ ubuntu-14.04-x86/scripts/base.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+perl -p -i -e 's#http://us.archive.ubuntu.com/ubuntu#http://mirror.rackspace.com/ubuntu#gi' /etc/apt/sources.list
+
 # Update the box
 apt-get -y update >/dev/null
 apt-get -y install facter linux-headers-$(uname -r) build-essential zlib1g-dev libssl-dev libreadline-gplv2-dev curl unzip >/dev/null
