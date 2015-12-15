c170a72b6b1ac043775bd4265e1168f3c27889c3
Mathias Larsen <tripox@tripox.dk>
2015-12-15 01:16:25 +0100

    Downgrade puppet

--- ubuntu-12.04-x86_64/scripts/puphpet.sh
+++ ubuntu-12.04-x86_64/scripts/puphpet.sh
@@ -8,7 +8,7 @@ mkdir -p '/.puphpet-stuff/shell'
 wget --quiet --tries=5 --connect-timeout=10 --no-check-certificate -O '/.puphpet-stuff/shell/os-detect.sh' 'https://raw.githubusercontent.com/puphpet/puphpet/master/archive/puphpet/shell/os-detect.sh'
 wget --quiet --tries=5 --connect-timeout=10 --no-check-certificate -O '/.puphpet-stuff/shell/initial-setup.sh' 'https://raw.githubusercontent.com/puphpet/puphpet/master/archive/puphpet/shell/initial-setup.sh'
 wget --quiet --tries=5 --connect-timeout=10 --no-check-certificate -O '/.puphpet-stuff/shell/install-ruby.sh' 'https://raw.githubusercontent.com/puphpet/puphpet/master/archive/puphpet/shell/install-ruby.sh'
-wget --quiet --tries=5 --connect-timeout=10 --no-check-certificate -O '/.puphpet-stuff/shell/install-puppet.sh' 'https://raw.githubusercontent.com/puphpet/puphpet/master/archive/puphpet/shell/install-puppet.sh'
+wget --quiet --tries=5 --connect-timeout=10 --no-check-certificate -O '/.puphpet-stuff/shell/install-puppet.sh' 'https://github.com/puphpet/puphpet/raw/d4c71db3a2363d724f36535b7cb48550f19fcb89/archive/puphpet/shell/install-puppet.sh'
 
 chmod +x '/.puphpet-stuff/shell/os-detect.sh'
 chmod +x '/.puphpet-stuff/shell/initial-setup.sh'
