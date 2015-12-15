23a5f4a102a8a2f3ee74e4a44bfcea650027f7b6
Mathias Larsen <tripox@tripox.dk>
2015-12-15 22:42:07 +0100

    Downgrade puppet

--- ubuntu-14.04-x86_64/scripts/puphpet.sh
+++ ubuntu-14.04-x86_64/scripts/puphpet.sh
@@ -8,15 +8,12 @@ mkdir -p '/.puphpet-stuff/shell'
 wget --quiet --tries=5 --connect-timeout=10 --no-check-certificate -O '/.puphpet-stuff/shell/os-detect.sh' 'https://raw.githubusercontent.com/puphpet/puphpet/master/archive/puphpet/shell/os-detect.sh'
 wget --quiet --tries=5 --connect-timeout=10 --no-check-certificate -O '/.puphpet-stuff/shell/initial-setup.sh' 'https://raw.githubusercontent.com/puphpet/puphpet/master/archive/puphpet/shell/initial-setup.sh'
 wget --quiet --tries=5 --connect-timeout=10 --no-check-certificate -O '/.puphpet-stuff/shell/install-ruby.sh' 'https://raw.githubusercontent.com/puphpet/puphpet/master/archive/puphpet/shell/install-ruby.sh'
-wget --quiet --tries=5 --connect-timeout=10 --no-check-certificate -O '/.puphpet-stuff/shell/install-puppet.sh' 'https://github.com/puphpet/puphpet/raw/d4c71db3a2363d724f36535b7cb48550f19fcb89/archive/puphpet/shell/install-puppet.sh'
 
 chmod +x '/.puphpet-stuff/shell/os-detect.sh'
 chmod +x '/.puphpet-stuff/shell/initial-setup.sh'
 chmod +x '/.puphpet-stuff/shell/install-ruby.sh'
-chmod +x '/.puphpet-stuff/shell/install-puppet.sh'
 
 source '/.puphpet-stuff/shell/initial-setup.sh' '/.puphpet-stuff'
 source '/.puphpet-stuff/shell/install-ruby.sh'
-source '/.puphpet-stuff/shell/install-puppet.sh'
 
 apt-get -y install bindfs
