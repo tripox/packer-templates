269d6996ce0eda0464be0e79466e438604c28ea2
Mathias Larsen <tripox@tripox.dk>
2015-12-15 22:42:22 +0100

    Fix vmware tools installation

--- ubuntu-14.04-x86_64/scripts/vmware.sh
+++ ubuntu-14.04-x86_64/scripts/vmware.sh
@@ -7,7 +7,6 @@ fi
 
 # Install the VMWare Tools from a linux ISO.
 
-#wget http://192.168.0.185/linux.iso -P /tmp
 mkdir -p /mnt/vmware
 mount -o loop /home/vagrant/linux.iso /mnt/vmware
 
@@ -17,5 +16,5 @@ tar xzf /mnt/vmware/VMwareTools-*.tar.gz
 umount /mnt/vmware
 rm -fr /home/vagrant/linux.iso
 
-/tmp/vmware-tools-distrib/vmware-install.pl -d
+echo 'yes' | /tmp/vmware-tools-distrib/vmware-install.pl -d
 rm -fr /tmp/vmware-tools-distrib
